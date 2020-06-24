ea3c6ea7be17c69f8ddb7b962139794d641a0a92
nereg <okv23200@gmail.com>
2020-06-24 16:20:16 +0300

    One more thing

--- src/config.example.py
+++ src/config.example.py
@@ -1,7 +1,10 @@
 class Config:
     def __init__(self):
-        self.dbPath = '../main.db' # db path relative to src folder
-        self.adminId = '' # discord id of bot admin
-        self.token = '' # bot's token
-        self.defaultPrefix = '!' # default bot's commands prefix
+        self.dbHost = 'db'
+        self.dbUser = 'root'
+        self.dbPass = 'secret'
+        self.DB = 'bot'
+        self.adminId = ''
+        self.token = ''
+        self.defaultPrefix = '!'
         self.inviteUrl = 'https://discord.com/oauth2/authorize?client_id=713272720053239808&scope=bot&permissions=8' #fix permisions!
