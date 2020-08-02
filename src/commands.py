bc1da393acac77b0e8765d10fa43291b5b3c55a7
nereg <okv23200@gmail.com>
2020-08-02 21:14:40 +0300

    Added !info command

--- src/commands.py
+++ src/commands.py
@@ -6,6 +6,10 @@ import menus as m
 import server_cmd as server # import /server command module (see server_cmd.py)
 import json
 import config
+import datetime
+import psutil
+from psutil._common import bytes2human
+import statistics 
 
 class BulkCommands(commands.Cog):
     def __init__(self,bot):
@@ -48,6 +52,28 @@ List of added servers :
 {servers}
         ''')
     
+    @commands.command()
+    async def info(self, ctx):
+        time = datetime.datetime(2000,1,1,0,0,0,0)
+        RAM = f'{bytes2human(psutil.virtual_memory().used)}/{bytes2human(psutil.virtual_memory().total)}'
+        meUser = self.bot.get_user(277490576159408128)
+        embed = discord.Embed(title=f'Info about {self.bot.user.name}',timestamp=time.utcnow())
+        embed.set_footer(text=f'Bot v0.1 • GPLv3 • Requested by {ctx.author.name}')
+        embed.add_field(name='<:Link:739476980004814898> Invite link',value='[Here!](https://bit.ly/ARKTop)',inline=True)
+        embed.add_field(name='<:Github:739476979631521886> GitHub',value='[Here!](https://github.com/Nereg/ARKMonitorBot)',inline=True)
+        embed.add_field(name='<:Discord:739476979782254633> Support server',value='[Here!](https://discord.gg/QbSDHtq)',inline=True)
+        embed.add_field(name='<:DB:739476980075986976> Servers in database',value=f'{makeRequest("SELECT Id FROM servers").__len__()}',inline=True)
+        embed.add_field(name='<:RAM:739511277600112640> RAM',value=RAM,inline=True)
+        embed.add_field(name='<:Commit:739476980013072384> Commit',value='[`IAMLAZYSHIT`](https://github.com/Nereg/ARKMonitorBot)',inline=True)
+        embed.add_field(name=':ping_pong: Ping',value=int(self.bot.latency * 1000),inline=True)
+        embed.add_field(name='<:me:739473644874367007> Creator',value=f'{meUser.name}#{meUser.discriminator}',inline=True)
+        embed.add_field(name='<:Discord:739476979782254633> Currently in',value=f'{len(self.bot.guilds)} servers',inline=True)
+        embed.add_field(name='<:Role:739476980076118046> Role on this server',value=f'@{ctx.me.roles[1].name}',inline=True)
+        embed.add_field(name=':grey_exclamation: Current prefix',value=f'{get_prefix(1,ctx.message)}',inline=True)
+        embed.add_field(name='<:Cpu:739492057990693005> Current CPU utilisation',value=f'{round(statistics.mean(psutil.getloadavg()),1)}',inline=True)
+        await ctx.send(embed=embed)
+
+
     @commands.command()
     async def ping(self,ctx):
         time = int(self.bot.latency * 1000)
