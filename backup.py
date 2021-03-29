a20bcf59043bef7045accb179472423dd734a6d3
nereg <okv23200@gmail.com>
2021-03-29 16:11:02 +0300

    This should fix problem with the script

--- backup.py
+++ backup.py
@@ -10,7 +10,7 @@ day = time.strftime('%d-%H') # get and format time
 container_name = 'arkmonitorbot_db_1' # set container name to use in commands
 password = cfg.dbPass # get DB password and user
 user = cfg.dbUser
-backup = f'docker exec {container_name} bash -c "touch /var/lib/mysql/{day}.sql && mysqldump -u {user} -p{password} bot > /var/lib/mysql/{day}.sql"' # dumps all tables of bot DB in {day.sql}
+backup = f'docker exec {container_name} bash -c "touch /var/lib/mysql/{day}.sql && mysqldump -u {user} -p{password} bot > /var/lib/mysql/{day}.sql --protocol=TCP"' # dumps all tables of bot DB in {day.sql}
 copy = f'mkdir -p ~/backup/{months} && docker cp {container_name}:/var/lib/mysql/{day}.sql ~/backup/{months}' # makes backup folder and copyes backup from container to that folder
 send = f'cd ~/backup/ && git add ~/backup/{months}/{day}.sql && git commit . -m "Backup from main server ({time.strftime("%Y.%m.%d.%H")})" && git push --set-upstream origin master' # commints and pushes backed up copy of DB
 os.system(backup) # run constucted commands
