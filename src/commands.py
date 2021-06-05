679db719512a6a6766a91cdbae7a39642302e110
Nereg <47698366+Nereg@users.noreply.github.com>
2021-06-05 11:45:25 +0300

    Added more protections and async to !info and !list

--- src/commands.py
+++ src/commands.py
@@ -24,10 +24,10 @@ class BulkCommands(commands.Cog):
         l = c.Translation() # load translation
         GuildId = ctx.guild.id
         Type = 0 # lefovers of old code (supposed support for DM's)
-        data = await makeAsyncRequest('SELECT * FROM settings WHERE GuildId=%s AND Type=%s',(GuildId,Type)) # get srttings of current guild
+        data = await makeAsyncRequest('SELECT * FROM settings WHERE GuildId=%s AND Type=%s',(GuildId,Type)) # get settings of current guild
         notifications = await makeAsyncRequest('SELECT * FROM notifications WHERE Type=3') # select server notifications
         watchedServers = [] # list of watched servers
-        # most bugged and required part of the bot AFAIK
+        # most bugged and required part of the bot AFAIK (no updater is lol)
         for notification in notifications: # for notifications 
             if (notification[1] ==  ctx.channel.id): # if bot will notify in current channel (not in some channel of the guild and it is a new feature to make)
                 watchedServers.append(json.loads(notification[4])) # # append server id in list
@@ -40,9 +40,9 @@ class BulkCommands(commands.Cog):
         else:
             Servers = json.loads(data[0][3]) #remove()
         statement = "SELECT * FROM servers WHERE Id IN ({})".format(', '.join(['{}'.format(Servers[i]) for i in range(len(Servers))]))
-        data = makeRequest(statement)
+        data = await makeAsyncRequest(statement)
         i = 1 # i (yeah classic)
-        for result in data: # fro each record in DB
+        for result in data: # from each record in DB
             #print(result)
             #print(result[0] in watchedServers[0])
             #print(watchedServers)
@@ -64,12 +64,13 @@ class BulkCommands(commands.Cog):
         # send message
         await ctx.send(f''' 
 List of added servers :
-{servers}
+{discord.utils.escape_mentions(servers)}
         ''')
     
     @commands.bot_has_permissions(add_reactions=True,read_messages=True,send_messages=True,manage_messages=True,external_emojis=True)
     @commands.command()
     async def info(self, ctx):
+        count = await makeAsyncRequest("SELECT COUNT(Id) FROM servers")
         time = datetime.datetime(2000,1,1,0,0,0,0)
         RAM = f'{bytes2human(psutil.virtual_memory().used)}/{bytes2human(psutil.virtual_memory().total)}'
         meUser = self.bot.get_user(277490576159408128)
@@ -79,7 +80,7 @@ List of added servers :
         embed.add_field(name='<:Link:739476980004814898> Invite link',value='[Here!](https://bit.ly/ARKTop)',inline=True)
         embed.add_field(name='<:Github:739476979631521886> GitHub',value='[Here!](https://github.com/Nereg/ARKMonitorBot)',inline=True)
         embed.add_field(name='<:Discord:739476979782254633> Support server',value='[Here!](https://bit.ly/ARKDiscord)',inline=True)
-        embed.add_field(name='<:DB:739476980075986976> Servers in database',value=f'{makeRequest("SELECT COUNT(Id) FROM servers")[0][0]}',inline=True)
+        embed.add_field(name='<:DB:739476980075986976> Servers in database',value=f'{count[0][0]}',inline=True)
         embed.add_field(name='<:RAM:739476925852155914> RAM',value=RAM,inline=True)
         embed.add_field(name='<:Bot:748958111456296961> Version',value=self.cfg.version,inline=True)
         embed.add_field(name=':ping_pong: Ping',value=f'{int(self.bot.latency * 1000)} ms',inline=True)
@@ -88,7 +89,7 @@ List of added servers :
         embed.add_field(name='<:Role:739476980076118046> Role on this server',value=role,inline=True)
         embed.add_field(name=':grey_exclamation: Current prefix',value=f'{await get_prefix(1,ctx.message)}',inline=True)
         embed.add_field(name='<:Cpu:739492057990693005> Current CPU utilisation',value=f'{round(statistics.mean(psutil.getloadavg()),1)}',inline=True)
-        message = makeRequest('SELECT * FROM settings WHERE GuildId=1')
+        message = await makeAsyncRequest('SELECT * FROM settings WHERE GuildId=1')
         if (message.__len__() <= 0):
             message = 'No current message'
         else: 
