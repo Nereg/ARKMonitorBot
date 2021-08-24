8d9707095cbb2836da6cd78e33ce5ac66d1877d5
Nereg <47698366+Nereg@users.noreply.github.com>
2021-08-24 17:01:40 +0300

    The problem was in MySql + debug now works in my a2s fork

--- src/updater.py
+++ src/updater.py
@@ -99,10 +99,10 @@ class NeoUpdater(commands.Cog):
     # let's do anything normal __init__ can't do 
     async def init(self): 
         self.httpSession = aiohttp.ClientSession()  # for use in http API's
-        #self.sqlPool = await aiomysql.create_pool(host=self.cfg.dbHost, port=3306,  # somehow works see:
+        self.sqlPool = await aiomysql.create_pool(host=self.cfg.dbHost, port=3306,  # somehow works see:
                                                   # https://github.com/aio-libs/aiomysql/issues/574
-                                                  #user=self.cfg.dbUser, password=self.cfg.dbPass,
-                                                  #db=self.cfg.DB, loop=asyncio.get_running_loop(), minsize=self.workersCount)
+                                                  user=self.cfg.dbUser, password=self.cfg.dbPass,
+                                                  db=self.cfg.DB, loop=asyncio.get_running_loop(), minsize=2)
         # add debug plugin to list of plugins
         self.plugins.append(DebugPlugin(self))
         # add Battlemetrics plugin
@@ -149,14 +149,14 @@ class NeoUpdater(commands.Cog):
     # performs SQL request using aiomysql pool instead of regular function
     async def makeAsyncRequest(self, SQL, params=()):
         # hotfix !
-        return await makeAsyncRequest(SQL, params)
-        # conn = await self.sqlPool.acquire()  # acquire one connecton from the pool
-        # async with conn.cursor() as cur:  # with cursor as cur
-        #     await cur.execute(SQL, params)  # execute SQL with parameters
-        #     result = await cur.fetchall()  # fetch all results
-        #     await conn.commit()  # commit changes
-        # self.sqlPool.release(conn)  # release current connection to the pool
-        # return result  # return result
+        #return await makeAsyncRequest(SQL, params)
+        conn = await self.sqlPool.acquire()  # acquire one connecton from the pool
+        async with conn.cursor() as cur:  # with cursor as cur
+            await cur.execute(SQL, params)  # execute SQL with parameters
+            result = await cur.fetchall()  # fetch all results
+            await conn.commit()  # commit changes
+        self.sqlPool.release(conn)  # release current connection to the pool
+        return result  # return result
 
     async def performance(self,globalStart,globalStop,localStart,chunkTimes):
         # calculate global time 
